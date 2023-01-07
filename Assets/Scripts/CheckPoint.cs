bda6d6cc83935ca40e3e4f1b51f7a8e2837e5b4c
NoelJasik <Noeloskar99@gmail.com>
2023-01-07 22:13:20 +0100

    Checkpoint working!

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
+    DeathManager deathManager;
+    Animator anim;
+    
+    [SerializeField]
+    Transform checkpointSpawnPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        deathManager = FindObjectOfType<DeathManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(checkpointSpawnPoint.position != deathManager.respawnPoint)
+        {
+            anim.Play("Unused");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if(other.tag == "Player")
+        {
+            deathManager.SetCheckpoint(checkpointSpawnPoint.position);
+            anim.Play("Reached");
+        }
     }
 }
