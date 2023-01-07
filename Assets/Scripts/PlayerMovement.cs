09bdb8c4d04e7ddaa9f5cf3dde530b58f9ed6d94
NoelJasik <Noeloskar99@gmail.com>
2023-01-07 01:49:43 +0100

    Changed to base particles on spead not input

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -53,7 +53,7 @@ public class PlayerMovement : MonoBehaviour
         // If the player is moving forward, spawn a particle effect
             foreach (GameObject particle in movementEffect)
             {
-                particle.SetActive(verticalInput > 0);
+                particle.SetActive(currentSpeed > 0);
             }
 
         // If the current speed is less than the maximum speed, apply a force in the forward direction based on the input
